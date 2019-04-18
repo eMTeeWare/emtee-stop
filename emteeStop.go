e5920d1b9d45296bf82fecf3e0c18fcd83009026
Merlin Thomas <dev@emteeware.net>
2019-04-18 21:26:08 +0200

    Remove unnecessary arrays from struct

--- emteeStop.go
+++ emteeStop.go
@@ -41,20 +41,20 @@ func main() {
 			Station string `xml:"station,attr"`
 			Stops   []struct {
 				Id        string `xml:"id,attr"`
-				TrainData []struct {
+				TrainData struct {
 					F           string `xml:"f,attr"`
 					Type        string `xml:"t,attr"`
 					Owner       string `xml:"o,attr"`
 					TrainClass  string `xml:"c,attr"`
 					TrainNumber int    `xml:"n,attr"`
 				} `xml:"tl"`
-				Arrival []struct {
+				Arrival struct {
 					PlannedTime     string `xml:"pt,attr"`
 					PlannedPlatform string `xml:"pp,attr"`
 					TrainLine       string `xml:"l,attr"`
 					PlannedPath     string `xml:"ppth,attr"`
 				} `xml:"ar"`
-				Departure []struct {
+				Departure struct {
 					PlannedTime     string `xml:"pt,attr"`
 					PlannedPlatform string `xml:"pp,attr"`
 					TrainLine       string `xml:"l,attr"`
