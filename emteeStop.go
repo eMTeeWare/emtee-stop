f71c0db0e1b3d895154065fb1bf318f7e643b452
Merlin Thomas <emteeware@gmail.com>
2019-04-25 20:31:44 +0200

    Extract timetable struct to dedicated file

--- emteeStop.go
+++ emteeStop.go
@@ -37,32 +37,6 @@ func main() {
 		data, _ := ioutil.ReadAll(response.Body)
 		responseString := normalizeXml(data)
 
-		type Timetable struct {
-			Station string `xml:"station,attr"`
-			Stops   []struct {
-				Id        string `xml:"id,attr"`
-				TrainData struct {
-					F           string `xml:"f,attr"`
-					Type        string `xml:"t,attr"`
-					Owner       string `xml:"o,attr"`
-					TrainClass  string `xml:"c,attr"`
-					TrainNumber int    `xml:"n,attr"`
-				} `xml:"tl"`
-				Arrival struct {
-					PlannedTime     string `xml:"pt,attr"`
-					PlannedPlatform string `xml:"pp,attr"`
-					TrainLine       string `xml:"l,attr"`
-					PlannedPath     string `xml:"ppth,attr"`
-				} `xml:"ar"`
-				Departure struct {
-					PlannedTime     string `xml:"pt,attr"`
-					PlannedPlatform string `xml:"pp,attr"`
-					TrainLine       string `xml:"l,attr"`
-					PlannedPath     string `xml:"ppth,attr"`
-				} `xml:"dp"`
-			} `xml:"s"`
-		}
-
 		var timetable Timetable
 		err = xml.Unmarshal([]byte(responseString), &timetable)
 		if err != nil {
