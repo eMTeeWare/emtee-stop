98834d9ae51e12b621967750cd638eb42bc096a7
Merlin Thomas <dev@emteeware.net>
2019-05-30 22:31:41 +0200

    Add struct for simpler representation of a trip

---
+++ trip.go
@@ -0,0 +1,11 @@
+package main
+
+type Trip struct {
+	Id              string
+	PlannedTime     string
+	PlannedPlatform string
+	TrainLine       string
+	PlannedPath     string
+	ActualTime      string
+	ActualPlatform  string
+}
